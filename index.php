532736051b50b6a015ebcbf4130672bf4356d861
jpratik15 <60647886+jpratik15@users.noreply.github.com>
2020-10-01 21:34:11 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Index</title>
+</head>
+<body>
+<h1>Login</h1>
+    <!-- Taking Credentials -->
+    <form action="login.php" method="post">
+        
+        <label for="Username">Username: </label>
+        <input type="text" name="username"><br><br>
+
+        <label for="Password">Password: </label>
+        <input type="password" name="password"><br><br>
+
+        <input type="submit" class="buttons">
+
+    </form>
+
+    
+</body>
+</html>
